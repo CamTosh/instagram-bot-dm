739d5ccc6bc8d939539169f8582b7d7fb47ea820
Kowshika N <66908744+kowshika-n@users.noreply.github.com>
2020-09-29 03:16:44 +0530

    updated selectors

--- instadm.py
+++ instadm.py
@@ -12,37 +12,38 @@ DEFAULT_IMPLICIT_WAIT = 30
 
 
 class InstaDM(object):
-    
+
     def __init__(self, username, password, headless=True, instapy_workspace=None, profileDir=None):
         self.selectors = {
-          "home_to_login_button": ".WquS1 a",
-          "username_field": "username",
-          "password_field": "password",
-          "button_login": "._0mzm-",
-          "search_user": "queryBox",
-          "select_user": "._0mzm-",
-          "textarea": "textarea",
-          "send": "button"
+            "home_to_login_button": "//button[text()='Log In']",
+            "username_field": "username",
+            "password_field": "password",
+            "button_login": "//button/*[text()='Log In']",
+            "search_user": "queryBox",
+            "select_user": "//img[@data-testid='user-avatar']",
+            "next_button": "//button/*[text()='Next']",
+            "textarea": "//textarea[@placeholder]",
+            "send": "//button[text()='Send']"
         }
 
         # Selenium config
         options = webdriver.ChromeOptions()
-        
+
         if profileDir:
-            options.add_argument("user-data-dir=profiles/" + profileDir) 
+            options.add_argument("user-data-dir=profiles/" + profileDir)
 
         if headless:
             options.add_argument("--headless")
-        
+
         mobile_emulation = {
             "userAgent": 'Mozilla/5.0 (Linux; Android 4.0.3; HTC One X Build/IML74K) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
         }
-        options.add_experimental_option("mobileEmulation", mobile_emulation)                         
+        options.add_experimental_option("mobileEmulation", mobile_emulation)
 
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_position(0, 0)
         self.driver.set_window_size(414, 736)
-        
+
         # Instapy init DB
         self.instapy_workspace = instapy_workspace
         self.conn = None
@@ -77,15 +78,16 @@ class InstaDM(object):
         # homepage
         self.driver.get('https://instagram.com')
         self.__random_sleep__(3, 5)
-        self.driver.find_elements_by_css_selector(self.selectors['home_to_login_button'])[0].click()
-        self.__random_sleep__(2, 4)
-        
+        if self.__wait_for_element__(self.selectors['home_to_login_button'], 'xpath', 10):
+            self.__get_element__(self.selectors['home_to_login_button'], 'xpath').click()
+        self.__random_sleep__(5, 7)
+
         # login
         logging.info('Login with {}'.format(username))
         self.__scrolldown__()
         self.driver.find_element_by_name(self.selectors['username_field']).send_keys(username)
         self.driver.find_element_by_name(self.selectors['password_field']).send_keys(password)
-        self.driver.find_elements_by_css_selector(self.selectors['button_login'])[2].click()
+        self.__get_element__(self.selectors['button_login'], 'xpath').click()
         self.__random_sleep__()
 
     def sendMessage(self, user, message):
@@ -95,28 +97,30 @@ class InstaDM(object):
 
         self.__wait_for_element__(self.selectors['search_user'], "name")
         self.__type_slow__(self.selectors['search_user'], "name", user)
-        self.__random_sleep__()
+        self.__random_sleep__(7, 10)
 
         # Select user
-        elements = self.driver.find_elements_by_css_selector(self.selectors['select_user'])
-        elements[2].click()
-        self.__random_sleep__()
-        
-        # Go to page
-        elements[0].click()
-        self.__random_sleep__()
-        self.__wait_for_element__("*//textarea", "xpath")
-        self.__type_slow__("(*//textarea)[1]", "xpath", message)
-        self.__random_sleep__()
+        elements = self.driver.find_elements_by_xpath(self.selectors['select_user'])
+        if elements and len(elements) > 0:
+            elements[0].click()
+            self.__random_sleep__()
 
-        buttons = self.driver.find_elements_by_css_selector(self.selectors['send'])
-        buttons[len(buttons)-1].click()
+        # Go to page
+        if self.__wait_for_element__(self.selectors['next_button'], "xpath"):
+            self.__get_element__(self.selectors['next_button'], "xpath").click()
+            self.__random_sleep__()
+
+        if self.__wait_for_element__(self.selectors['textarea'], "xpath"):
+            self.__type_slow__(self.selectors['textarea'], "xpath", message)
+            self.__random_sleep__()
+
+        if self.__wait_for_element__(self.selectors['send'], "xpath"):
+            self.__get_element__(self.selectors['send'], "xpath").click()
 
         if self.conn is not None:
             self.cursor.execute('INSERT INTO message (username, message) VALUES(?, ?)', (user, message))
             self.conn.commit()
-
-        self.__random_sleep__()
+        self.__random_sleep__(50, 60)
 
     def sendGroupMessage(self, users, message):
         logging.info(f'Send group message {message} to {users}')
@@ -133,18 +137,22 @@ class InstaDM(object):
             self.__random_sleep__()
 
             # Select user
-            elements = self.driver.find_elements_by_css_selector(self.selectors['select_user'])
-            elements[2].click()
-            self.__random_sleep__(2, 4)
+            elements = self.driver.find_elements_by_xpath(self.selectors['select_user'])
+            if elements and len(elements) > 0:
+                elements[0].click()
+                self.__random_sleep__()
 
         # Go to page
-        elements[0].click()
-        self.__random_sleep__()
-        self.__wait_for_element__("*//textarea", "xpath")
-        self.__type_slow__("(*//textarea)[1]", "xpath", message)
-        self.__random_sleep__()
-        buttons = self.driver.find_elements_by_css_selector(self.selectors['send'])
-        buttons[len(buttons)-1].click()
+        if self.__wait_for_element__(self.selectors['next_button'], "xpath"):
+            self.__get_element__(self.selectors['next_button'], "xpath").click()
+            self.__random_sleep__()
+
+        if self.__wait_for_element__(self.selectors['textarea'], "xpath"):
+            self.__type_slow__(self.selectors['textarea'], "xpath", message)
+            self.__random_sleep__()
+
+        if self.__wait_for_element__(self.selectors['send'], "xpath"):
+            self.__get_element__(self.selectors['send'], "xpath").click()
 
         if self.conn is not None:
             self.cursor.executemany("""
@@ -223,7 +231,7 @@ class InstaDM(object):
             actions.click(element).perform()
             for s in input_text:
                 element.send_keys(s)
-                sleep(uniform(0.1, 0.2))
+                sleep(uniform(0.25, 0.75))
 
         except Exception as e:
             logging.error(e)
@@ -237,4 +245,6 @@ class InstaDM(object):
     def __scrolldown__(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-
+    def teardown(self):
+        self.driver.close()
+        self.driver.quit()
