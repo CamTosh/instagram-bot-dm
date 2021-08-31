ca9476f597d5226f591e33ba85891125d9536701
Amar Tabakovic <tabakovicamar1@gmail.com>
2021-08-31 18:19:25 +0200

    accept_cookies from 'Accept' to 'Accept All' Instagram's accept cookie selector now has the text 'Accept All' instead of just 'Accept'

--- instadm.py
+++ instadm.py
@@ -16,7 +16,7 @@ class InstaDM(object):
 
     def __init__(self, username, password, headless=True, instapy_workspace=None, profileDir=None):
         self.selectors = {
-            "accept_cookies": "//button[text()='Accept']",
+            "accept_cookies": "//button[text()='Accept All']",
             "home_to_login_button": "//button[text()='Log In']",
             "username_field": "username",
             "password_field": "password",
