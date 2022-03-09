cde4f8451305b260b47399b776f93c3a416eed8b
Danila Vershinin <ciapnz@gmail.com>
2022-03-09 13:41:59 +0300

    Fix pip package for #19 (#37)
    * PyPi support
    
    * v3.5 no longer available
    
    * Only check installability
    
    * Make setuptools happy
    
    * Update setup.py
    
    * Fix pip package
    
    Co-authored-by: Toche Camille <tochecamille@gmail.com>

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
-from setuptools import find_packages, setup
-from io import open
 import os
-import re
+from io import open
 
-_version_re = re.compile(r"__version__\s=\s'(.*)'")
+from setuptools import setup
 
 install_requires = ['webdriver_manager', 'selenium']
-with open("readme.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 base_dir = os.path.dirname(__file__)
 
-with open(os.path.join(base_dir, "__about__.py"), 'r') as f:
-    version = _version_re.search(f.read()).group(1)
-
 setup(
     name="instadm",
-    version=version,
+    version='0.0.2',
     author="Toche Camille",
     author_email="tochecamille@gmail.com",
     url="https://github.com/CamTosh/instagram-bot-dm",
     description="Instagram bot to send direct messages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=["tests", "docs"]),
+    py_modules=["instadm"],
     zip_safe=False,
     license="GPLv3",
     install_requires=install_requires,
