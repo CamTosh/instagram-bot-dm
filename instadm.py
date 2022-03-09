5f852ab208a8b09ae2651db62206f260ed0f2e4c
Blendica Vlad <vlad.blendica@consultant2000.ro>
2022-03-10 00:05:45 +0200

    Update instadm.py

--- instadm.py
+++ instadm.py
@@ -127,14 +127,14 @@ class InstaDM(object):
 
         if self.__wait_for_element__(self.selectors['send'], "xpath"):
             element = self.__get_element__('rh7Wz', 'CLASS')
-                if element is not None:
-                    self.driver.execute_script("""
+            if element is not None:
+                self.driver.execute_script("""
                                     var element = arguments[0];
                                     element.parentNode.removeChild(element);
                                     """, element)
-                element = self.__get_element__('vohlx', 'CLASS')
-                if element is not None:
-                    self.driver.execute_script("""
+            element = self.__get_element__('vohlx', 'CLASS')
+            if element is not None:
+                self.driver.execute_script("""
                                     var element = arguments[0];
                                     element.parentNode.removeChild(element);
                                     """, element)
